e99d835ff133cf0da20a0dd7518ab49ff16ead04
Jeff Leung <badjeff@gmail.com>
2026-01-30 15:07:27 +0800

    add mode switching in mlx90393 driver

--- config/boards/shields/samukun/drivers/input/input_mlx90393.c
+++ config/boards/shields/samukun/drivers/input/input_mlx90393.c
@@ -4,7 +4,58 @@
  */
 
 /*
-Sample initial log:
+
+## Overview
+
+This is a MLX90393 driver for Zephye Input Sub-system.
+
+- I2C/SPI Bus Compitable
+- Auto-calibration for dead zone
+- Intrrupt with on-chip Wake-On Change Mode
+- Auto-switch to full-res Burst Mode
+- Downshift to Wake-on Change mode
+
+
+## Device tree node sample:
+
+&pinctrl {
+    spi2_default: spi2_default {
+        group1 {
+            psels = <NRF_PSEL(SPIM_SCK, 0, 8)>,
+                    <NRF_PSEL(SPIM_MOSI, 0, 20)>,
+                    <NRF_PSEL(SPIM_MISO, 0, 17)>;
+        };
+    };
+    spi2_sleep: spi2_sleep {
+        group1 {
+            psels = <NRF_PSEL(SPIM_SCK, 0, 8)>,
+                    <NRF_PSEL(SPIM_MOSI, 0, 20)>,
+                    <NRF_PSEL(SPIM_MISO, 0, 17)>;
+            low-power-enable;
+        };
+    };
+};
+
+#include <dt-bindings/spi/spi.h>
+&spi2 {
+    compatible = "nordic,nrf-spim";
+    status = "okay";
+    pinctrl-0 = <&spi2_default>;
+    pinctrl-1 = <&spi2_sleep>;
+    pinctrl-names = "default", "sleep";
+    cs-gpios = <&gpio1 4 (GPIO_ACTIVE_LOW | GPIO_PULL_UP)>;
+    spi_mlx90393: mlx90393@0 {
+        compatible = "melexis,mlx90393-input";
+        status = "okay";
+        spi-max-frequency = <1000000>;
+        reg = <0>;
+        irq-gpios = <&gpio1 6 (GPIO_ACTIVE_HIGH | GPIO_PULL_DOWN)>;
+        no-z;
+    };
+};
+
+
+## Sample initial log:
 
 [00:00:00.554,382] <inf> input_mlx90393: Initializing MLX90393 on SPI bus
 [00:00:00.554,412] <inf> input_mlx90393: set mask_zyxt: 0xF6
@@ -18,6 +69,8 @@ Sample initial log:
 [00:00:00.955,322] <inf> input_mlx90393: read 0x01 config status: 0x01
 [00:00:00.955,413] <inf> input_mlx90393: wake-on change mode status: 0x41
 [00:00:00.955,413] <inf> input_mlx90393: MLX90393 input driver initialized successfully
+
+
 */
 
 #define DT_DRV_COMPAT melexis_mlx90393_input
@@ -49,7 +102,8 @@ struct mlx90393_config {
 	const struct gpio_dt_spec irq_gpio;
     bool no_x, no_y, no_z;
     uint16_t calib_cycle;
-    uint16_t woc_thd_xy, woc_thd_z;
+    uint16_t ex_woc_thd_xy, ex_woc_thd_z;
+    uint16_t downshift;
     uint16_t rpt_dzn_x, rpt_dzn_y, rpt_dzn_z;
 };
 
@@ -67,6 +121,8 @@ struct mlx90393_data {
     int32_t max_x, max_y, max_z;
     int16_t thd_xy; // 0x07h: WOXY_THRESHOLD [15:0] (signed)
     int16_t thd_z;  // 0x08h: WOZ_THRESHOLD [15:0] (signed)
+    bool in_woc_mode, in_burst_mode;
+    uint16_t burst_in_deadzone;
 };
 
 static int mlx90393_cmd_read(const struct device *dev,
@@ -141,12 +197,18 @@ static int mlx90393_reset(const struct device *dev) {
     return ret;
 }
 
-static int mlx90393_exit_burst_mode(const struct device *dev) {
+static void mlx90393_set_current_mode(const struct device *dev, bool in_woc_mode, bool in_burst_mode) {
+    struct mlx90393_data *data = dev->data;
+    data->in_woc_mode = in_woc_mode;
+    data->in_burst_mode = in_burst_mode;
+}
+
+static int mlx90393_exit_mode(const struct device *dev) {
     // const struct mlx90393_config *config = dev->config;
     // struct mlx90393_data *data = dev->data;
+    int ret = 0;
     uint8_t cmd;
     uint8_t status;
-    int ret;
 
     cmd = 0x80; // Exit mode
     ret = mlx90393_cmd_read(dev, &cmd, 1, &status, 1);
@@ -160,30 +222,60 @@ static int mlx90393_exit_burst_mode(const struct device *dev) {
         return -EINVAL;
     }
     LOG_INF("exit status: 0x%02x", status);
+    mlx90393_set_current_mode(dev, false, false);
 
+    // k_msleep(1); // if BURST_DATA_RATE down to zero, wait at least 1ms
+
+    return ret;
+}
+
+static int mlx90393_start_burst_mode(const struct device *dev) {
+    // const struct mlx90393_config *config = dev->config;
+    struct mlx90393_data *data = dev->data;
+    int ret = 0;
+    if (!data->in_burst_mode) {
+        uint8_t cmd;
+        uint8_t status;
+
+        cmd = 0x1E & data->mask_zyxt; // Start Burst Mode for zyx
+        ret = mlx90393_cmd_read(dev, &cmd, 1, &status, 1);
+
+        if (ret < 0) {
+            LOG_WRN("Failed to read burst mode status: %d", ret);
+            return ret;
+        }
+        else if (status & MLX90383_STATUS_ERR_BITS) {
+            LOG_ERR("read burst mode: 0x%02X", status);
+            return -EINVAL;
+        }
+        LOG_INF("burst mode status: 0x%02x", status);
+        mlx90393_set_current_mode(dev, false, true);
+    }
     return ret;
 }
 
 static int mlx90393_start_woc_mode(const struct device *dev) {
     // const struct mlx90393_config *config = dev->config;
     struct mlx90393_data *data = dev->data;
-    uint8_t cmd;
-    uint8_t status;
-    int ret;
+    int ret = 0;
+    if (!data->in_woc_mode) {
+        uint8_t cmd;
+        uint8_t status;
 
-    cmd = 0x2E & data->mask_zyxt; // Start Wake-up on Change Mode for zyx
-    ret = mlx90393_cmd_read(dev, &cmd, 1, &status, 1);
+        cmd = 0x2E & data->mask_zyxt; // Start Wake-up on Change Mode for zyx
+        ret = mlx90393_cmd_read(dev, &cmd, 1, &status, 1);
 
-    if (ret < 0) {
-        LOG_WRN("Failed to read wake-on change mode status: %d", ret);
-        return ret;
+        if (ret < 0) {
+            LOG_WRN("Failed to read wake-on change mode status: %d", ret);
+            return ret;
+        }
+        else if (status & MLX90383_STATUS_ERR_BITS) {
+            LOG_ERR("read wake-on change mode: 0x%02X", status);
+            return -EINVAL;
+        }
+        LOG_INF("wake-on change mode status: 0x%02x", status);
+        mlx90393_set_current_mode(dev, true, false);
     }
-    else if (status & MLX90383_STATUS_ERR_BITS) {
-        LOG_ERR("read wake-on change mode: 0x%02X", status);
-        return -EINVAL;
-    }
-    LOG_INF("wake-on change mode status: 0x%02x", status);
-
     return ret;
 }
 
@@ -230,7 +322,7 @@ static int mlx90393_set_config(const struct device *dev) {
 }
 
 static int mlx90393_set_woc_threshold(const struct device *dev) {
-    // const struct mlx90393_config *config = dev->config;
+    const struct mlx90393_config *config = dev->config;
     struct mlx90393_data *data = dev->data;
 
     LOG_INF("setting woc threshold xy:%d z:%d", data->thd_xy, data->thd_z);
@@ -272,7 +364,7 @@ static int mlx90393_set_woc_threshold(const struct device *dev) {
 
     reg_config[0] = 0x60;  // Write register command
     reg_config[1] = 0x10;  // AH: WOC_DIFF
-    reg_config[2] = 0x00;  // AL: BURST_DATA_RATE = DEFAULT (0x0)
+    reg_config[2] = 0x01;  // AL: BURST_DATA_RATE = N * 20ms
     reg_config[3] = 0x01 << 2;  // Select address register, (0x02 << 2)
     ret = mlx90393_cmd_read(dev, reg_config, 4, &status, 1);
 
@@ -308,9 +400,19 @@ static void mlx90393_work_handler(struct k_work *work) {
         goto reenable_irq;
     }
     // Convert the data (big endian MSB:LSB for each axis)
-    x = data->mask_zyxt & BIT(1) ? (int16_t)sys_get_be16(&read_data[1]) : 0;
-    y = data->mask_zyxt & BIT(2) ? (int16_t)sys_get_be16(&read_data[3]) : 0;
-    z = data->mask_zyxt & BIT(3) ? (int16_t)sys_get_be16(&read_data[5]) : 0;
+    uint8_t _data_offset_ = 1;
+    if (data->mask_zyxt & BIT(1)) {
+        x = (int16_t)sys_get_be16(&read_data[_data_offset_]);
+        _data_offset_ += 2;
+    }
+    if (data->mask_zyxt & BIT(2)) {
+        y = (int16_t)sys_get_be16(&read_data[_data_offset_]);
+        _data_offset_ += 2;
+    }
+    if (data->mask_zyxt & BIT(3)) {
+        z = (int16_t)sys_get_be16(&read_data[_data_offset_]);
+        // _data_offset_ += 2;
+    }
     
     if (!data->calibrated) {
         data->sum_x += x;
@@ -325,39 +427,38 @@ static void mlx90393_work_handler(struct k_work *work) {
         data->calibra_cnt++;
 
         if ((data->calibra_cnt % config->calib_cycle) == 0) {
-            LOG_INF("Calibration %d/50 - Current: X:%d Y:%d Z:%d", data->calibra_cnt, x, y, z);
+            LOG_INF("calibrating %d/%d: x:%d y:%d z:%d", data->calibra_cnt, config->calib_cycle, x, y, z);
         }
 
         if (data->calibra_cnt >= config->calib_cycle) {
             data->org_x = data->sum_x / config->calib_cycle;
             data->org_y = data->sum_y / config->calib_cycle;
             data->org_z = data->sum_z / config->calib_cycle;
-            LOG_INF("calibrated org: x=%6d y=%6d z=%6d", data->org_x, data->org_y, data->org_z);
+            LOG_INF("calibrated org: x:%d y:%d z:%d", data->org_x, data->org_y, data->org_z);
 
             data->thd_xy = MAX(data->max_x - data->min_x, data->max_y - data->min_y);
             data->thd_z = (data->max_z - data->min_z);
-            LOG_INF("min/max: X:%6d/%6d Y:%6d/%6d Z:%6d/%6d", 
+            LOG_INF("min/max: x:%d/%d y:%d/%d z:%d/%d", 
                     (int)data->min_x, (int)data->max_x, 
                     (int)data->min_y, (int)data->max_y, 
                     (int)data->min_z, (int)data->max_z);
 
             //
-            // trim woc threshold to calibrated result
-            // dealing with 3 facts: noise reduction, resolution, power efficiency
+            // convert woc threshold to calibrated result
+            // tune `thd_trim` with 3 facts: noise reduction, resolution, power efficiency
             //
-            data->thd_xy = ( data->thd_xy * 1/2 ) + config->woc_thd_xy;
-            data->thd_z = ( data->thd_z * 1/2 ) + config->woc_thd_z;
+            float thd_trim = 0.89; // normal distribution trim
+            data->thd_xy = ( data->thd_xy * thd_trim ) + config->ex_woc_thd_xy;
+            data->thd_z = ( data->thd_z * thd_trim ) + config->ex_woc_thd_z;
             LOG_INF("thd xy:%6d z:%6d", data->thd_xy, data->thd_z);
 
             data->calibrated = true;
 
-            ret = mlx90393_exit_burst_mode(dev);
+            ret = mlx90393_exit_mode(dev);
             if (ret < 0) {
-                LOG_ERR("Failed to exit burst mode: %d", ret);
+                LOG_ERR("Failed to exit mode: %d", ret);
                 goto reenable_irq;
             }
-
-            k_msleep(100);
 
             ret = mlx90393_set_woc_threshold(dev);
             if (ret < 0) {
@@ -365,6 +466,9 @@ static void mlx90393_work_handler(struct k_work *work) {
                 goto reenable_irq;
             }
 
+            //
+            // when calibration done and enter WOC mode to reduce power;
+            //
             ret = mlx90393_start_woc_mode(dev);
             if (ret < 0) {
                 LOG_ERR("Failed to start wake-on change mode: %d", ret);
@@ -383,12 +487,12 @@ static void mlx90393_work_handler(struct k_work *work) {
     if (abs(dy) < (int)config->rpt_dzn_y) dy = 0;
     if (abs(dz) < (int)config->rpt_dzn_z) dz = 0;
 
-    // LOG_DBG("raw: X:%6d Y:%6d Z:%6d | origin: X:%6d Y:%6d Z:%6d | crd: X:%6d Y:%6d Z:%6d", 
+    // LOG_DBG("raw x:%6d y:%6d z:%6d | org x:%d y:%d z:%d | crd z:%6d y:%6d z:%6d",
     //         x, y, z, data->org_x, data->org_y, data->org_z, dx, dy, dz);
-    // LOG_DBG("min/max: X:%6d/%6d Y:%6d/%6d Z:%6d/%6d", 
+    // LOG_DBG("min/max: x:%d/%d y:%d/%d z:%d/%d", 
     //         (int)data->min_x, (int)data->max_x, (int)data->min_y, (int)data->max_y, 
     //         (int)data->min_z, (int)data->max_z);
-    // LOG_DBG("thd xy:%6d z:%6d", data->thd_xy, data->thd_z);
+    // LOG_DBG("thd xy:%d z:%d", data->thd_xy, data->thd_z);
 
     if (dx || dy || dz) {
         if (dx) {
@@ -401,6 +505,62 @@ static void mlx90393_work_handler(struct k_work *work) {
             input_report_rel(dev, INPUT_REL_Z, dz, true, K_FOREVER);
         }
         LOG_DBG("delta x/y/z: %6d / %6d / %6d", dx, dy, dz);
+    }
+
+    bool in_deadzone = !dx && !dy && !dz;
+
+    //
+    // when the most recent readings are all within the dead zone of the downshift interval.
+    // this means the joystick has returned to the neutral position, switching back to WOC mode;
+    //
+    if (in_deadzone && data->in_burst_mode) {
+
+        data->burst_in_deadzone = data->burst_in_deadzone + 1;
+
+        #if CONFIG_ZMK_LOG_LEVEL >= LOG_LEVEL_DBG
+        if (data->burst_in_deadzone % (config->downshift/3) == 0) {
+            LOG_DBG("burst_in_deadzone: %d", data->burst_in_deadzone);
+        }
+        #endif
+
+        if (data->burst_in_deadzone >= config->downshift) {
+            data->burst_in_deadzone = 0;
+
+            ret = mlx90393_exit_mode(dev);
+            if (ret < 0) {
+                LOG_ERR("Failed to exit mode: %d", ret);
+                goto reenable_irq;
+            }
+
+            ret = mlx90393_start_woc_mode(dev);
+            if (ret < 0) {
+                LOG_ERR("Failed to start burst mode: %d", ret);
+                goto reenable_irq;
+            }
+        }
+
+        goto reenable_irq;
+    }
+
+    //
+    // when WOC intrrupt triggered, switch to burst most to max out the resolution;
+    //
+    if (!in_deadzone && !data->in_burst_mode) {
+
+        ret = mlx90393_exit_mode(dev);
+        if (ret < 0) {
+            LOG_ERR("Failed to exit mode: %d", ret);
+            goto reenable_irq;
+        }
+
+        ret = mlx90393_start_burst_mode(dev);
+        if (ret < 0) {
+            LOG_ERR("Failed to start burst mode: %d", ret);
+            goto reenable_irq;
+        }
+        // LOG_WRN("BURST MODE!");
+
+        goto reenable_irq;
     }
 
 reenable_irq:
@@ -474,6 +634,9 @@ static int mlx90393_init(const struct device *dev) {
     data->min_x = data->min_y = data->min_z = INT32_MAX;
     data->max_x = data->max_y = data->max_z = INT32_MIN;
     data->thd_xy = data->thd_z = 0;
+    data->in_woc_mode = false;
+    data->in_burst_mode = false;
+    data->burst_in_deadzone = 0;
 
     uint8_t mask_zyxt = 0xFE;
     if (config->no_x) mask_zyxt ^= BIT(1);
@@ -482,9 +645,9 @@ static int mlx90393_init(const struct device *dev) {
     data->mask_zyxt = mask_zyxt;
     LOG_INF("set mask_zyxt: 0x%02X", data->mask_zyxt);
 
-    ret = mlx90393_exit_burst_mode(dev);
+    ret = mlx90393_exit_mode(dev);
     if (ret < 0) {
-        LOG_ERR("Failed to exit burst mode: %d", ret);
+        LOG_ERR("Failed to exit mode: %d", ret);
         return ret;
     }
 
@@ -548,8 +711,9 @@ static int mlx90393_init(const struct device *dev) {
         .no_y = DT_INST_PROP(n, no_y),                                                            \
         .no_z = DT_INST_PROP(n, no_z),                                                            \
         .calib_cycle = DT_INST_PROP(n, calib_cycle),                                              \
-        .woc_thd_xy = DT_INST_PROP(n, woc_thd_xy),                                                \
-        .woc_thd_z = DT_INST_PROP(n, woc_thd_z),                                                  \
+        .ex_woc_thd_xy = DT_INST_PROP(n, ex_woc_thd_xy),                                          \
+        .ex_woc_thd_z = DT_INST_PROP(n, ex_woc_thd_z),                                            \
+        .downshift = DT_INST_PROP(n, downshift),                                                  \
         .rpt_dzn_x = DT_INST_PROP(n, rpt_dzn_x),                                                  \
         .rpt_dzn_y = DT_INST_PROP(n, rpt_dzn_y),                                                  \
         .rpt_dzn_z = DT_INST_PROP(n, rpt_dzn_z),                                                  \
